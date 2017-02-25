c4282f5d4f03fb7ce1b1a3d092f304c2f3deedb4
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2017-02-24 21:40:43 -0500

    Build tool complete

--- YACSwrapper/Builder/builder.py
+++ YACSwrapper/Builder/builder.py
@@ -1,4 +1,21 @@
+import urllib2
+import json
+
+
+
 def importYACScourses():
+    url = "https://yacs.cs.rpi.edu/api/v5/courses"
+    response = urllib2.urlopen(url)
+    result = json.load(response)
+
+    list_of_course_ids = []
+
+    with open("../Alexa/LIST_OF_COURSES.txt","w") as f:
+        for course in result["courses"]:
+            f.write(course["name"]+"\n")
 
 
-    return "stuff"
+
+
+if __name__ == "__main__":
+    importYACScourses()
